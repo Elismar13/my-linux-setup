5bb33ab4c7496afd974608106c1232c63265753b
Elismar Silva <elismarsilva.5@outlook.com>
2025-07-22 17:28:02 -0300

    feat: add installation step for btop in the setup script

--- scripts/install_htop.sh
+++ scripts/install_htop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "📊 Installing btop..."
+
+sudo apt install btop
+
+echo "✅ btop installed."
+
 echo "📊 Installing htop..."
 
-sudo apt install -y htop
+sudo apt install htop
 
 echo "✅ htop installed."
